d88106fa74655858a35a0187d2c49ab56f976fe7
Pablo Macuada <pablo.macuada@usach.cl>
2023-06-12 02:26:23 -0400

    Se crea implementación de diseño

--- Class/Node_Operation.cpp
+++ Class/Node_Operation.cpp
@@ -4,33 +4,24 @@ Node_Operation::Node_Operation(char operation, Node *left, Node *right) {
     this->operation = operation;
     this->left = left;
     this->right = right;
-    this->parent = nullptr;
-    type = OPERATOR;
+    type = OPERATION;
 }
 
 Node_Operation::Node_Operation(char operation) {
     this->operation = operation;
-    left = nullptr;
-    right = nullptr;
-    parent = nullptr;
-    type = OPERATOR;
-    visited = false;
+    type = OPERATION;
 }
 
 Node_Operation::~Node_Operation() {
-    if (left !=nullptr) {
-        delete left;
-    }
-    
-    if (right != nullptr) { 
-        delete right;
-    }
+    //std::cout << "Node_Operation destructor" << std::endl;
 }
 
 void Node_Operation::print() {
-    std::cout << operation << std::endl;
-    std::cout << "---IZQ----"<<std::endl;
-    left->print();
-    std::cout << "---DER----"<<std::endl;
-    right->print();
+    std::cout << "╔════════════════ Node Operation ════════════════" << std::endl;
+    std::cout << "║ Operation: " << operation << std::endl;
+    std::cout << "║ Type: " << type << std::endl;
+    std::cout << "║ Parent: " << parent << std::endl;
+    std::cout << "║ Left: " << left << std::endl;
+    std::cout << "║ Right: " << right << std::endl;
+    std::cout << "╚════════════════ End Operation ════════════════" << std::endl;
 }
--- Class/Node_Operation.h
+++ Class/Node_Operation.h
@@ -2,12 +2,13 @@
 
 class Node_Operation : public Node {
 public:
+    Node *left;
+    Node *right;
+    char operation;
+
+    
     Node_Operation(char operation, Node *left, Node *right);
     Node_Operation(char operation); // caso de no conocer hijos
     ~Node_Operation();
-    Node *left;
-    Node *right;
-    Node *parent;
-    char operation;
     void print();
 };
